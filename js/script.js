b4d65c9fc2d72fbb8f37a011a9d6e79a31e37efd
MJulia111 <julia.m241992@gmail.com>
2021-02-09 22:30:35 +0300

    updated

--- js/script.js
+++ js/script.js
@@ -25,7 +25,9 @@ document.querySelector(".blog-page__btn").addEventListener("click", (e) => {
 	sendRequest('GET', requestURL)
 		.then((result) => {
 			for (let i = 0; i < 6; i++) {
-				parent.innerHTML += '<article class="blog-page__card card-blog"><div class="card-blog__body"><h2 class="card-blog__title"><a href="#" class="card-blog__title-link">' + result[i].title + '</a></h2><div class="card-blog__text"><p>' + result[i].body.substring(0, 110) + '<span>...</span></p></div><a href="#" class="card-blog__more"><span>Read More</span></a></div></article>'
+				parent.innerHTML += '<article class="blog-page__card card-blog"><div class="card-blog__body"><h2 class="card-blog__title"><a href="#" class="card-blog__title-link">'
+					+ result[i].title + '</a></h2><div class="card-blog__text"><p>'
+					+ result[i].body.substring(0, 110) + '<span>...</span></p></div><a href="#" class="card-blog__more"><span>Read More</span></a></div></article>'
 			}
 		})
 		.catch(err => console.error('Что-то пошло не так...', err))
